a2b82cc920b9fb7d22111062cfffee913386d621
Daina40 <daina9438@gmail.com>
2023-12-30 22:40:31 +0600

    'cloud'

--- account/models.py
+++ account/models.py
@@ -4,29 +4,29 @@ from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, Permis
 # Create your models here.
 
 class UserProfileManager(BaseUserManager):
-    def create_user(self, email, name, password=None):
-        if not email:
-            raise ValueError('Users must have an email address.')
-        email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+    def create_user(self, phone, name, password=None):
+        if not phone:
+            raise ValueError('Users must have an phone number.')
+        phone = self.normalize_email(phone)
+        user = self.model(phone=phone, name=name)
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self,email,name,password):
-        user = self.create_user(email, name, password)
+    def create_superuser(self,phone,name,password):
+        user = self.create_user(phone, name, password)
         user.is_superuser = True
         user.is_staff = True
 
         user.save(using = self._db)
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(max_length=20, unique=True)
+    phone = models.CharField(max_length=20, unique=True, default=None)
     name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default = False)
     objects = UserProfileManager()
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'phone'
     REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
